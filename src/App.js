fadf429e7f20a0dc59cebc404b90048a3dd6bba2
31Bharath <you@example.com>
2023-11-08 01:16:35 +0530

    zostelhomes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Destination from './Pages/Destination'
 import Navbar from './Components/Navbar'
 import Franchise from './Pages/Franchise'
 import Footer from './Components/Footer'
+import ZostelHomes from './Pages/ZostelHomes'
 
 export default function App() {
   return (
@@ -12,9 +13,10 @@ export default function App() {
     <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/destinations' element={<Destination />}></Route>
-      <Route path='/franchise'  element={<Franchise />}></Route>
+      <Route path='/' element={<Home />}/>
+      <Route path='/destinations' element={<Destination />}/>
+      <Route path='/franchise'  element={<Franchise />}/>
+      <Route path='/zostel-homes' element={<ZostelHomes/>}/>
     </Routes>
     <Footer />
     </BrowserRouter>
